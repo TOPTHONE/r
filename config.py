c293d41cb2ee41f6519cebbd391b2e4ee9368cda
TOPTHONE <126317055+TOPTHONE@users.noreply.github.com>
2023-02-26 13:07:25 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# ها وردة جاي تخمـط الملف كتابة - @topthone1 @topthone
+
+# Don't use quotes( " and " ) - [اختياري]
+
+APP_ID= ""
+API_HASH=
+STRING_SESSION= ""
+TG_BOT_TOKEN= ""
+TG_BOT_USERNAME= ""
+ENV= "ANYTHING"
+TZ= "Asia/Baghdad"
+DATABASE_URL= ""
+
+# [اجباري]
